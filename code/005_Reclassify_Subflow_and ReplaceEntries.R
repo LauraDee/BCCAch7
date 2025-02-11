0895c0ae81e566e38e8dea74019732e19ae8b0dd
LauraDee <Laura.dee@colorado.edu>
2025-02-11 10:11:06 -0700

    trying to rebind

--- code/005_Reclassify_Subflow_and ReplaceEntries.R
+++ code/005_Reclassify_Subflow_and ReplaceEntries.R
@@ -10,34 +10,39 @@ library(data.table)
 setwd("/Users/lade8828/Library/CloudStorage/OneDrive-UCB-O365/Documents/GitHub/BCCAch7/data")
 docs <- read.csv("cleaned_4_byFlow.csv")
 
-#load look up tables 
+#load look up tables
 #need to figure out what to do with the FLOW itself that have been changed
 biotic <- read.csv("./data_cleaning/bioticFlowSubtype_lookup.csv")
 socio <- read.csv("./data_cleaning/socioculturalFlowSubtype_lookup.csv")
 phys <-  read.csv("./data_cleaning/physicalFlowSubtype_lookup.csv")
 
+#combine subflows look ups into a single datafile - **this didnt work**
+identical(names(biotic), names(phys))
+glimpse(biotic)
+glimpse(phys)
+subflows <- rbind(biotic, phys)
+subflows <- rbind(socio, biotic)
+
 #Read in new entries that will be appended and assigned new IDs
-# replace <- read.csv 
+# replace <- read.csv
 
 # in the look up files, probably need to first remove the ones removed in the 004 step to avoid confusion
-phys <- phys %>% filter(Citation != "Chen, 2011, Journal of Sustainable Development", 
-                        Citation != "Nielsen, 2015, Global Change Biology", 
+phys <- phys %>% filter(Citation != "Chen, 2011, Journal of Sustainable Development",
+                        Citation != "Nielsen, 2015, Global Change Biology",
                         Citation != "Vaddey, 2010, Watershed Management",
                         Citation != "Jenkins et al., 2013, Advances in Parasitology",
                         Citation != "Dube et al., 2012, INTEGRATED ENVIRONMENTAL ASSESSMENT AND MANAGEMENT", #Mabel checking
                         Citation != "Covich et al., 1997, Hydrological Processes",
                         Citation != "de la Fontaine,  2018, Ecology",
-                        Citation != "Perry, 2007, Climate Change 2007") 
+                        Citation != "Perry, 2007, Climate Change 2007")
 
 biotic <- biotic %>% filter(Citation != "Noyes, 2009, Environment International",
                             Citation != "de la Fontaine,  2018, Ecology",
-                            Citation != "Perry, 2007, Climate Change 2007") 
+                            Citation != "Perry, 2007, Climate Change 2007")
 
-socio <- socio %>% filter(Citation != "Shin et al., 2021, Global Change Biology")                    
+socio <- socio %>% filter(Citation != "Shin et al., 2021, Global Change Biology")
 
-#combine subflows look ups into a single datafile - **this didnt work**
-subflows <- rbind(biotic, phys)
-subflows <- rbind(socio, phys)
+
 
 # Merge the new subflows into the main dataset
 updated_docs <- merge(subflows, docs, by = "ID_DOI_by_Flow", all.y = T)
@@ -47,7 +52,7 @@ glimpse(updated_docs)
 #updated_docs[X2.2.Subtype.NEW == "range shifts", X2.2.Subtype.NEW := "Range shift"]
 #updated_docs[X2.2.Subtype.NEW == "range shift", X2.2.Subtype.NEW := "Range shift"]
 
-# Add in the new entries entirely for: 
+# Add in the new entries entirely for:
   # de la Fontaine,  2018, Ecology
   # Costa, D., 2021, Journal of Great Lakes Research
   # Shin et al., 2021, Global Change Biology
