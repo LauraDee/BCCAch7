6c59c5273d02b35d01c18971ae4387dc9549109f
LauraDee <Laura.dee@colorado.edu>
2025-02-11 10:17:05 -0700

    Replace entries with recoded ones
    #Read in new entries that will be appended and assigned new IDs
    # we will need to add all of the same columns since the main data has the extra management ones, or we should do it this in an earlier step
    could use your help
    
    Co-Authored-By: Bernardo Bastien <38734147+BerBastien@users.noreply.github.com>

--- code/005_Reclassify_Subflow_and ReplaceEntries.R
+++ code/005_Reclassify_Subflow_and ReplaceEntries.R
@@ -16,32 +16,33 @@ biotic <- read.csv("./data_cleaning/bioticFlowSubtype_lookup.csv")
 socio <- read.csv("./data_cleaning/socioculturalFlowSubtype_lookup.csv")
 phys <-  read.csv("./data_cleaning/physicalFlowSubtype_lookup.csv")
 
+#Read in new entries that will be appended and assigned new IDs
+# we will need to add all of the same columns since the main data has the extra management ones, or we should do it this in an earlier step
+# replace_socio <- read.csv()
+
 #combine subflows look ups into a single datafile - **this didnt work**
 identical(names(biotic), names(phys))
+identical(names(biotic), names(socio))
 glimpse(biotic)
 glimpse(phys)
+glimpse(socio)
+
 subflows <- rbind(biotic, phys)
 subflows <- rbind(socio, biotic)
 
-#Read in new entries that will be appended and assigned new IDs
-# replace <- read.csv
+
 
 # in the look up files, probably need to first remove the ones removed in the 004 step to avoid confusion
-phys <- phys %>% filter(Citation != "Chen, 2011, Journal of Sustainable Development",
+subflows <- subflows %>% filter(Citation != "Chen, 2011, Journal of Sustainable Development",
                         Citation != "Nielsen, 2015, Global Change Biology",
                         Citation != "Vaddey, 2010, Watershed Management",
-                        Citation != "Jenkins et al., 2013, Advances in Parasitology",
+                        Citation != "Jenkins et al., 2013, Advances in Parasitology", #Enrique recoding
                         Citation != "Dube et al., 2012, INTEGRATED ENVIRONMENTAL ASSESSMENT AND MANAGEMENT", #Mabel checking
-                        Citation != "Covich et al., 1997, Hydrological Processes",
-                        Citation != "de la Fontaine,  2018, Ecology",
-                        Citation != "Perry, 2007, Climate Change 2007")
-
-biotic <- biotic %>% filter(Citation != "Noyes, 2009, Environment International",
-                            Citation != "de la Fontaine,  2018, Ecology",
-                            Citation != "Perry, 2007, Climate Change 2007")
-
-socio <- socio %>% filter(Citation != "Shin et al., 2021, Global Change Biology")
-
+                        Citation != "Covich et al., 1997, Hydrological Processes", #Mabel recoding
+                        Citation != "de la Fontaine,  2018, Ecology", #Colleen recoded
+                        Citation != "Noyes, 2009, Environment International", #Becky recoding
+                        Citation != "Perry, 2007, Climate Change 2007",
+                        Citation != "Shin et al., 2021, Global Change Biology") #Kyle recoded
 
 
 # Merge the new subflows into the main dataset
