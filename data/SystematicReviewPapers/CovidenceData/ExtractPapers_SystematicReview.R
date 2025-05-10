ca5180979d1c2c3e021a714e4cd3b3681674df91
LauraDee <Laura.dee@colorado.edu>
2025-05-10 09:39:13 -0600

    extract final papers for coding

--- data/SystematicReviewPapers/CovidenceData/ExtractPapers_SystematicReview.R
+++ data/SystematicReviewPapers/CovidenceData/ExtractPapers_SystematicReview.R
@@ -21,7 +21,7 @@ biotic_full_v1 = fread('biotic_FULL_nov1extraction.csv', header = T)
 doc = fread('november1codingextraction.csv', header=T)
 final = fread('review_462040_included_csv_20250424044203.csv', header = T)
 
-#first identify the biotic papers that weren't sampled in the first round
+#fir  st identify the biotic papers that weren't sampled in the first round
 #and print that list
 # compare biotic csv - biotic_full_v1 vs biotic_v1
 # 
@@ -74,13 +74,18 @@ docs$`Original study` = NULL
 docs$`Human movement` = NULL
 
 #write out full reshaped data file 
-# write.csv(docs, "extractedpapers_May92025.csv")
+# write.csv(docs, "full_extractedpapers_May92025.csv")
+write.csv(docs, "new_only_extractedpapers_May92025.csv")
 
 #Before filtering the data, write out these files for other teams to look at:
 intervention = docs[Intervention == "TRUE",]
 write.csv(intervention, "intervention_May9_extraction.csv")
-IK = docs[IK == "TRUE",]
-write.csv(IK, "IK_May9extraction.csv")
+
+# IK = docs[IK == "TRUE",]
+# write.csv(IK, "IK_May9extraction.csv")
+
+transboundary_unclear = docs[`Transboundary unclear` == "TRUE",]
+write.csv(transboundary_unclear, "transboundary_unclear_May9.csv")
 
 #write out original study files to check whether they should be excluded
 og = docs[Original_study == "TRUE",] 
@@ -89,16 +94,16 @@ write.csv(og, "og_check_finalmay9.csv")
 ## Now filter to allocate per group 
 #numbers of tags per flow type (including tags in multiple types 
 # this number drops considerably below)
-table(docs$Sociocultural) #83 (10 co-tagged with human movement)
-table(docs$Biotic) # 392 .... (with 26 co-tagged with sociocultural)
-table(docs$Biophysical) #151 - final 
+table(docs$Sociocultural) #82 originally, now 83 where is the missing one (10 co-tagged with human movement)
+table(docs$Biotic) #additional 58...? 392 .... (with 27 co-tagged with sociocultural)
+table(docs$Biophysical) #151 - final is additional 24
 table(docs$Human_movement)  #51 
 
 # counts of co-tags out of curiosity 
 docs[, socio_human :=  Sociocultural == "TRUE" & Human_movement == "TRUE",]
 docs[, socio_biotic :=  Sociocultural == "TRUE" & Biotic == "TRUE",]
 table(docs$socio_human) #10 are co-tagged so we can allocate these to one person
-table(docs$socio_biotic) #26 are co-tagged with biotic! 
+table(docs$socio_biotic) #27 are co-tagged with biotic! 
 docs[, socio_physical :=  Sociocultural == "TRUE" & Biophysical == "TRUE",]
 table(docs$socio_physical) #only 5 that are also physical 
 docs[, human_biotic :=  Human_movement == "TRUE" & Biotic == "TRUE",]
@@ -116,31 +121,54 @@ table(human$Human_movement) #41
 write.csv(human, "human_May9extraction.csv")
 
 # Next step: exclude sociocultural (already done above) and human movement, then filter with ANY physical flow
-docs3 = docs2[Human_movement == "FALSE",]
-physical = docs3[Biophysical == "TRUE",]
-table(physical$Biophysical) #112 
+docs3 = docs[Human_movement == "FALSE",]
+physical = docs[Biophysical == "TRUE",]
+table(physical$Biophysical) #112  --> #142 --> 30 left. in the final... but it shoudlnt include original so this isnt working
 #how many left are physical co-tagged with biotic?
 physical[, biotic_physical :=  Biotic == "TRUE" & Biophysical == "TRUE",]
-table(physical$biotic_physical) #31 co-tagged. 
+table(physical$biotic_physical) #31 vs 35  ... 4 in final co-tagged. 
 write.csv(physical, "physical_May9extraction.csv")
 
 # Next step: exclude all but biotic then write out biotic
-biotic = docs3[Biophysical == "FALSE",] 
-biotic = biotic[Biotic == "TRUE",]
-head(biotic)
-table(biotic$Biotic) #256 
-write.csv(biotic, "biotic_May9extraction.csv")
+biotic2 = docs[Biophysical == "FALSE",] 
+biotic2 = biotic[Biotic == "TRUE",]
+head(biotic2)
+table(biotic2$Biotic) #256 --> 54 in the new that wasnt in the old
 
-### Randomly sample either 50% of remaining biotic or n= 200
-# https://dplyr.tidyverse.org/reference/sample_n.html 
-nrow(biotic) #256
-set.seed(7)
-biotic.sample = sample_n(biotic, 200, replace = FALSE)
-#test  
-nrow(biotic.sample)
 
-# could filter these 4 out first before sampling -
-table(biotic.sample$Original_study) # 2 are original study
-table(biotic.sample$`Perspective paper`) # 2 are perspective paper
-#OR:
-#biotic.50percent = sample_frac(biotic, 0.25, replace = FALSE)
+#combine biotic2 and biotic -- need to expand to add tags to biotic
+docs = biotic
+tags = unique(trimws(unlist(strsplit(docs$Tags, split=";"))))
+for(tag in tags) {
+  set(docs, j=tag, value=F)
+  set(docs, i=grep(tag, docs$Tags), j=tag, value=T)
+}
+setcolorder(docs, 
+            neworder=c("Sociocultural",
+                       "Human movement",
+                       "Biophysical",
+                       "Biotic",
+                       "IK",
+                       "Intervention",
+                       "Potentially relevant",
+                       "save for other sections",
+                       "Transboundary unclear",
+                       "Original study",
+                       "Perspective paper"),
+            after = "Tags")
+
+#remain column names with spaces
+docs$Human_movement = docs$`Human movement`
+docs$Original_study = docs$`Original study`
+docs$`Original study` = NULL
+docs$`Human movement` = NULL
+
+head(docs)
+
+#combine the remaining biotic papers that havent been screened yet
+biotic_final <- rbind(docs, biotic2, fill = TRUE)
+length(unique(biotic_final$DOI)) #100 left
+
+write.csv(biotic_final, "biotic_May9extraction.csv")
+table(biotic_final$`Transboundary unclear`)
+
